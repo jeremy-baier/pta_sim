572a924a795662eabc65f76a3c087026628d0a9e
Jeremy Baier <baierj@oregonstate.edu>
2023-07-17 13:14:23 -0700

    adding check_chains.py

---
+++ pta_sim/check_chains.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Code for checking chain convergence
+import numpy as np
+import la_forge.core as co
+import la_forge
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', dest='dir', action='store',
+                    type=str, default='.',
+                    help='Directory of chain to check.')
+parser.add_argument('--hm', dest='hm', action='store_true',
+                    default=False,
+                    help='Whether or not run is a hypermodel run.')
+args = parser.parse_args()
+
+if hm:
+    hmc = co.HyperModelCore(chaindir=args.dir)
+
+elif hm is False:
+    c0 = co.Core(chaindir=args.dir)
+
+rhat, idx = la_forge.diagnostics.grubin(c0)
+print(rhat)
+
+
+
+
+
